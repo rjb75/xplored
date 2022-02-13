1d6bc70b1c002a56b6d349e62707b5f90e6070a3
Risat Haque <risathq@gmail.com>
2022-02-13 15:03:14 -0700

    chore: springboot initializer

---
+++ microservices/tplanner/src/main/java/seng/xplored/tplanner/TplannerApplication.java
@@ -0,0 +1,15 @@
+package seng.xplored.tplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TplannerApplication.class, args);
+		System.out.println("Hello World!");
+
+	}
+
+}
---
+++ microservices/tplanner/src/test/java/seng/xplored/tplanner/TplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package seng.xplored.tplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
