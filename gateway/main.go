89a52fb6f324517a44425363c756cfd1b31e2f45
Robert Brown <brown.rjb75@gmail.com>
2022-02-23 16:56:12 -0700

    feat: add inital static routes for frontend

--- gateway/main.go
+++ gateway/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/rjb75/xplored-api-gateway/routes"
+)
 
 func main() {
-	fmt.Print("Hello world!")
+
+	app := fiber.New()
+
+	err := godotenv.Load("../.env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	routes.RegisterRoutes(app)
+
+	SERVER_PORT := os.Getenv("GATEWAY_PORT")
+	port := fmt.Sprintf(":%s", SERVER_PORT)
+	app.Listen(port)
 }
