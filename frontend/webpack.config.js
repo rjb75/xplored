69cf563d2d1f4b9c90ff4d79cae5668c17f2f03b
Robert Brown <brown.rjb75@gmail.com>
2022-02-26 16:14:45 -0700

    feat: add hot reload for go to frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,88 @@
+const webpack = require("webpack");
+require("dotenv").config({ path: "../.env" });
+
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: process.env.FRONTEND_ENV_MODE,
+  entry: {
+    app: ["./src/index.tsx"],
+    vendor: ["react", "react-dom"],
+  },
+  watch: process.env.FRONTEND_ENV_MODE === "development",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "js/[name].bundle.js",
+    publicPath: "/",
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".scss"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          process.env.FRONTEND_ENV_MODE === "development"
+            ? "style-loader"
+            : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              esModule: false,
+              sourceMap: true,
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: "file-loader",
+        options: {
+          name:
+            process.env.ENV === "development"
+              ? "assets/[folder]/[name].[ext]"
+              : "assets/[sha512:hash:base64:7].[ext]",
+          esModule: false,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
+    new MiniCssExtractPlugin({
+      filename: `[name].[contenthash].css`,
+      chunkFilename: `[id].css`,
+    }),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico",
+      manifest: "./public/manifest.json",
+      robots: "./public/robots.txt",
+    }),
+  ],
+};
