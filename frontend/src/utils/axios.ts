cbb93efe9144f53459ea985a0700243dc3fe4772
Robert Brown <brown.rjb75@gmail.com>
2022-02-25 17:36:32 -0700

    feat: add axios and cookie utilities

---
+++ frontend/src/utils/axios.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosRequestConfig, AxiosRequestHeaders } from "axios";
+import { API_BASE } from "./APIConstants";
+import { getCookie } from "./CookieUtils";
+
+// default axios header values
+const axiosDefaultHeaders: AxiosRequestHeaders = {
+    "Content-type": "application/json"
+}
+
+// default axios config values
+const axiosDefaultConfig: AxiosRequestConfig = {
+    baseURL: API_BASE,
+    timeout: 2000,
+    headers: axiosDefaultHeaders
+}
+
+const axiosInstance = axios.create(axiosDefaultConfig)
+
+// append authorization header if cookie exists
+axiosInstance.interceptors.request.use((config : AxiosRequestConfig) => {
+    if(!config.headers) {
+        config.headers = axiosDefaultHeaders;
+    }
+
+    const token = getCookie('access_token');
+
+    if(token !== "") {
+        config.headers.Authorization = token;
+    }
+
+    return config
+})
+
+export default axiosInstance;
