6b0a154326467203ebf4be9092d2a678a79dfc93
Risat Haque <risathq@gmail.com>
2022-02-25 21:42:32 -0700

    feat: dining parser working

--- microservices/dining/main.go
+++ microservices/dining/main.go
@@ -1,37 +1,65 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/Risath18/xplored-dining/models"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 
 func main() {
-	app := fiber.New()
+//	app := fiber.New()
 
 	//Setting up Environment Variables
 	err := godotenv.Load("../../.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	 //GeoCode()
-	 //Places()
 
+	GetDiningOptions("461 Skyview Shores Manor", "Calgary", "Alberta", "1000", "Tim Hortons")
+	
 	//Formatting port
-	SERVER_PORT := os.Getenv("DINING_PORT")
-	port := fmt.Sprintf(":%s", SERVER_PORT)
-	app.Listen(port)
+	// SERVER_PORT := os.Getenv("DINING_PORT")
+	// port := fmt.Sprintf(":%s", SERVER_PORT)
+	// app.Listen(port)
 }
 
 
-func GeoCode(){
+func GetDiningOptions(street_address string, city string, country string, radius string, keyword string){
+	//Format Address
+	plusFormattedAddress := strings.ReplaceAll("+" + street_address, " ", "+")
+	plusFormattedCity := strings.ReplaceAll("+" + city, " ", "+")
+	plusFormattedCountry := strings.ReplaceAll("+" + country, " ", "+")
+
+	//Format Keyword
+	plusFormattedKeyword := strings.ReplaceAll("+" + keyword, " ", "+")
+
+	//Combine Address
+	address := fmt.Sprintf("%s,%s,%s", plusFormattedAddress, plusFormattedCity, plusFormattedCountry) 
+
+	//Calculate Longitude & Latitude
+	var lc models.Location_class = GeoCode(address)
+
+	//Get Dining Data
+	var Result models.Response = Places(lc, radius, plusFormattedKeyword)
+	fmt.Println(Result) //Debugging Print Statement
+}
+
+
+/*
+* GeoCode API to determine Longitude and Latitude of given ADDRESS
+* Returns a struct with Longitude and Latitude
+*/
+func GeoCode(address string) models.Location_class{
+	var lc models.Location_class
 	client := &http.Client{}
 	req, err := http.NewRequest(http.MethodGet, "https://maps.googleapis.com/maps/api/geocode/json?", nil)
 	if err != nil {
@@ -40,7 +68,7 @@ func GeoCode(){
 
 	// appending to existing query args
 	q := req.URL.Query()
-	q.Add("address", "461+Skyview+Shores+Manor,+Calgary,+AB") //Formatted Address with requred '+'
+	q.Add("address", address) //Formatted Address with requred '+'
 	q.Add("key", os.Getenv("DINING_API")) //API Key
 
 	// assign encoded query string to http request
@@ -49,7 +77,7 @@ func GeoCode(){
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error when sending request to the server")
-		return
+		return lc
 	}
 
 	defer resp.Body.Close()
@@ -58,27 +86,33 @@ func GeoCode(){
 		log.Fatal(err)
 	}
 
-	//fmt.Println(resp.Status)
-	fmt.Println(string(responseBody))
-	
-	//TODO: Parser Not working
-	// var rp GCR	
-	// json.Unmarshal([]byte(string(responseBody)), &rp)
-	// fmt.Printf("Lat: %s", rp)
+	var rp models.GCR	
+	json.Unmarshal([]byte(string(responseBody)), &rp)
+	lc = rp.Results[0].Geometry.Location
+	return lc
 }
 
-func Places(){
+/*
+* Places API to determine Dining Options in a fixed RADIUS, KEYWORD with given ADDRESS
+* Returns a struct with information of restaurants and status
+*/
+func Places(lc	models.Location_class, radius string, keyword string) models.Response {
+	var Result models.Response
 	client := &http.Client{}
 	req, err := http.NewRequest(http.MethodGet, "https://maps.googleapis.com/maps/api/place/nearbysearch/json?", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	//Format Coordinates
+	coords := fmt.Sprintf("%f, %f", lc.Lat, lc.Lng)
+
+
 	// appending to existing query args
 	q := req.URL.Query()
-	q.Add("location", "51.15718593079007, -113.96789149778814") //Longitude and Latitude
-	q.Add("radius", "3000") //in Meters
-	q.Add("keyword", "TimHortons") //Searching
+	q.Add("location", coords) //Longitude and Latitude
+	q.Add("radius", radius) //in Meters
+	q.Add("keyword", keyword) //Searching
 	q.Add("key", os.Getenv("DINING_API")) //API Key
 
 	// assign encoded query string to http request
@@ -87,7 +121,7 @@ func Places(){
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error when sending request to the server")
-		return
+		return Result
 	}
 
 	defer resp.Body.Close()
@@ -96,6 +130,8 @@ func Places(){
 		log.Fatal(err)
 	}
 
-	fmt.Println(resp.Status)
-	fmt.Println(string(responseBody))
+	//Formatting with Model
+	Result.Json = string(responseBody)
+	Result.Status = resp.Status
+	return Result
 }
--- microservices/dining/models/GeoCodeResponse.go
+++ microservices/dining/models/GeoCodeResponse.go
@@ -1,23 +1,24 @@
+package models
+
 type GCR struct {
 	Results []Result `json:"results"`
-	Status string `json:"status"`
-
+	Status  string   `json:"status"`
 }
-type Result struct{
-	Addresscomponent []string `json:"address_components"`
-	Formatted_address string `json:"formatted_address"`
-	Geometry Geometry_Class `json:"geometry"`
-	Place_id string `json:"place_id"`
-	Types []string `json:"types"`
+type Result struct {
+	Addresscomponent  []string       `json:"address_components"`
+	Formatted_address string         `json:"formatted_address"`
+	Geometry          Geometry_Class `json:"geometry"`
+	Place_id          string         `json:"place_id"`
+	Types             []string       `json:"types"`
 }
 type Geometry_Class struct {
-	Bounds string `json:"bounds"`
-	Location Location_class `json:"location"`  
-	Location_type string `json:"location_type"`  
-	Viewport string `json:"viewport"`  
+	Bounds        string         `json:"bounds"`
+	Location      Location_class `json:"location"`
+	Location_type string         `json:"location_type"`
+	Viewport      string         `json:"viewport"`
 }
 
 type Location_class struct {
-	Lat string `json:"lat"`
-	Lng string `json:"lng"`  
+	Lat float64 `json:"lat"`
+	Lng float64 `json:"lng"`
 }
