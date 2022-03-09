97f8d2003668f62a95b254e3744600a2f5ce9380
alexishamrak <alhamrak@shaw.ca>
2022-03-09 10:16:59 -0700

    Update main.go

--- microservices/reviews/main.go
+++ microservices/reviews/main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	// "context"
+	"context"
 	"fmt"
 	"log"
 	"os"
+
 	// "strconv"
 	// "strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 
+	// "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/mongo/readpref"
+
 	//"github.com/kr/pretty"
 	_ "github.com/lib/pq"
 )
-
 
 func main() {
 	app := fiber.New()
@@ -32,5 +36,23 @@ func main() {
 	SERVER_PORT := os.Getenv("REVIEWS_PORT")
 	port := fmt.Sprintf(":%s", SERVER_PORT)
 	app.Listen(port)
-}
 
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(REVIEWS_DB))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	//ping primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected and pinged")
+}
