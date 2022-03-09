5a735c2e924acc98fb268c4b9d8d0b23e9b09edb
alexishamrak <alhamrak@shaw.ca>
2022-03-09 15:59:50 -0700

    added changes

--- microservices/reviews/routes.go
+++ microservices/reviews/routes.go
@@ -3,12 +3,17 @@ package main
 import (
 	//	"fmt"
 
+	"github.com/alexishamrak/xplored-reviews/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func RegisterRoutes(app *fiber.App) {
 	app.Use(cors.New())
+
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+	v1.Get("/tags", handlers.SearchTag)
 
 	//Api Version Configuration
 	// microservice := app.Group("/reviews") // /api
