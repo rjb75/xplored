8518f3d4dbe270eecc034d0428d6bb89e484d4ef
nickknapton12 <nickknapton12@gmail.com>
2022-03-08 21:52:13 -0700

    Scoffolding for pages

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Axios from "axios";
+import React, { useState } from "react";
+
+const Home = ()  => {
+
+  return (
+    <div>
+        <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/pages/POI.tsx
@@ -0,0 +1,13 @@
+import Axios from "axios";
+import React, { useState } from "react";
+
+const POI = ()  => {
+
+  return (
+    <div>
+        <h1>POI Page</h1>
+    </div>
+  );
+}
+
+export default POI;
