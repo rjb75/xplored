a4157c0dfeb7c6e33a09c6eafd51f893c9560526
Dylan Wheeler <dwheeler1127@gmail.com>
2022-03-09 15:28:55 -0700

    feat: added ComponentTest page to router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';import './App.css';
-import { Routes, Route } from 'react-router-dom'
-import Login from './pages/Login';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import ComponentTest from "./pages/ComponentTest";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Xplored</h1>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Xplored</h1>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/componenttest" element={<ComponentTest />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
