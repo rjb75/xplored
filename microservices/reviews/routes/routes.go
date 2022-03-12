ad27f3f34d47cba4dc98f7884a986b5c266e587c
alexishamrak <alhamrak@shaw.ca>
2022-03-12 10:14:27 -0700

    updates to reviews microservice

--- microservices/reviews/routes/routes.go
+++ microservices/reviews/routes/routes.go
@@ -2,10 +2,12 @@ package routes
 
 import (
 	//	"fmt"
+	"strings"
 
 	"github.com/alexishamrak/xplored-reviews/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/kr/pretty"
 )
 
 func RegisterRoutes(app *fiber.App) {
@@ -15,11 +17,41 @@ func RegisterRoutes(app *fiber.App) {
 	microservice := app.Group("/reviews") // /api
 	api := microservice.Group("/api")
 	v1 := api.Group("/v1")
-	v1.Get("/tags", handlers.SearchTag)
+	// v1.Get("/tags", handlers.SearchTag)
 
-	// reviewsRoutes(v1)
+	reviewsRoutes(v1)
 }
 
-// func reviewsRoutes(v fiber.Router) {
-// 	v.Get("/:address/:keyword/:radius", GetReviewsOptions)
-// }
+func reviewsRoutes(v fiber.Router) {
+	println("inside reviews routes")
+	v.Get("/:tags", GetReviewsOptions)
+}
+
+/*
+* Function associated with Endpoint to get Reviews Data
+ */
+func GetReviewsOptions(c *fiber.Ctx) error {
+	println("Inside get reviews options")
+	data := reviewsOptions(c, c.Params("tags"))
+
+	errorStatus := "Success"
+
+	if len(data.Error()) <= 0 {
+		errorStatus = "Error"
+	}
+
+	return c.Status(200).JSON(fiber.Map{"status": errorStatus, "data": data})
+}
+
+/*
+* reviewsOptions with Given Information
+ */
+func reviewsOptions(c *fiber.Ctx, tags string) error {
+	//Format Address
+	plusFormattedTags := strings.ReplaceAll("+"+tags, " ", "+")
+	println("inside reviews options")
+	//Get Reviews Data
+	Result := handlers.SearchTag(c, plusFormattedTags)
+	pretty.Println("The result is %s", Result) //Debugging Print Statement
+	return Result
+}
