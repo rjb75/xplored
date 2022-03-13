9966b9a7876269a6ba77a7b0224cd0c0bc2619dd
alexishamrak <alhamrak@shaw.ca>
2022-03-13 15:06:33 -0600

    Completed microservice

--- microservices/reviews/handlers/handlers.go
+++ microservices/reviews/handlers/handlers.go
@@ -3,8 +3,8 @@ package handlers
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
+	"strings"
 
 	// "strconv"
 	// "strings"
@@ -12,6 +12,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -20,14 +21,46 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func SearchTag(c *fiber.Ctx, userTags string) error {
+type TagsQuery struct {
+	Tags string `query:"tags"`
+}
+
+type ResultStruct struct {
+	Id          primitive.ObjectID `json:"_id" bson:"_id"`
+	FirstName   string             `json:"first_name" bson:"first_name"`
+	LastName    string             `json:"last_name" bson:"last_name"`
+	UserID      string             `json:"user_id" bson:"user_id"`
+	Title       string             `json:"title" bson:"title"`
+	Stars       string             `json:"stars" bson:"stars"`
+	Tags        []string           `json:"tags" bson:"tags"`
+	Description string             `json:"description" bson:"description"`
+}
+
+type PostStruct struct {
+	FirstName   string   `json:"first_name" bson:"first_name"`
+	LastName    string   `json:"last_name" bson:"last_name"`
+	UserID      string   `json:"user_id" bson:"user_id"`
+	Title       string   `json:"title" bson:"title"`
+	Stars       string   `json:"stars" bson:"stars"`
+	Tags        []string `json:"tags" bson:"tags"`
+	Description string   `json:"description" bson:"description"`
+}
+
+func SearchTag(c *fiber.Ctx) error {
 	reviewsDB := os.Getenv("REVIEWS_DB")
-	println("Inside search tag")
+
+	q := new(TagsQuery)
+	err := c.QueryParser(q)
+	plusFormattedTags := strings.Split(q.Tags, " ")
+
+	if q.Tags == "" {
+		return c.Status(400).JSON(fiber.Map{"status": "fail", "type": "Request Error", "cause": "Tag(s) are required for query"})
+	}
 
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(reviewsDB))
 
 	if err != nil {
-		panic(err)
+		return c.Status(500).JSON(fiber.Map{"status": "fail", "type": "Server Error", "cause": "Couldn't connect to database"})
 	}
 
 	defer func() {
@@ -38,54 +71,152 @@ func SearchTag(c *fiber.Ctx, userTags string) error {
 
 	//ping primary
 	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
-		panic(err)
+		return c.Status(500).JSON(fiber.Map{"status": "fail", "type": "Server Error", "cause": "Couldn't connect to database"})
 	}
 
 	db := client.Database("user_reviews").Collection("reviews")
-	tags := userTags
-	// var result bson.M
+	tags := plusFormattedTags
+
+	for i, t := range tags {
+		tags[i] = "#" + t
+	}
+	pretty.Println(bson.D{{"tags", tags}})
 
 	findTags := options.Find()
-	// findTags.SetLimit(10)
-	var results []interface{}
-	cur, err := db.Find(context.TODO(), bson.D{{"tags", tags}}, findTags)
+	var results []ResultStruct
+
+	cur, err := db.Find(context.TODO(), bson.D{{"tags", tags[0]}}, findTags)
 
 	if err != nil {
-		log.Fatal(err)
+		return c.Status(500).JSON(fiber.Map{"status": err})
 	}
 
 	for cur.Next(context.TODO()) {
-		var elem interface{}
+		var elem ResultStruct
 		err := cur.Decode(&elem)
 		if err != nil {
-			log.Fatal(err)
+			return c.Status(500).JSON(fiber.Map{"status": err})
 		}
-
-		results = append(results, elem)
+		if IDMatch(elem.Tags, tags) {
+			results = append(results, elem)
+		}
 	}
 
 	if err := cur.Err(); err != nil {
-		log.Fatal(err)
+		return c.Status(500).JSON(fiber.Map{"status": err})
 	}
 
 	cur.Close(context.TODO())
 	fmt.Printf("Found multiple documents + %+v\n", results)
-	// err = db.FindOne(context.TODO(), bson.D{{"tags", tags}}).Decode(&result)
-	// if err == mongo.ErrNoDocuments {
-	// 	fmt.Printf("No document was found with the tag %s\n", tags)
-	// 	return c.Status(500).SendString("error")
-	// }
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// // jsonData, err := json.MarshalIndent(result, "", "    ")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(result)
 
 	fmt.Println("Successfully connected and pinged")
 
 	pretty.Print(results)
+
 	return c.Status(200).JSON(results)
+}
+
+func IDMatch(elementTags []string, queryTags []string) bool {
+
+	for _, query := range queryTags {
+		if !TagsContains(query, elementTags) {
+			return false
+		}
+	}
+	return true
+}
+
+func TagsContains(searchTag string, elementTags []string) bool {
+	for _, tag := range elementTags {
+		if tag == searchTag {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+	This function will search for all reviews in the database
+*/
+func AllReviews(c *fiber.Ctx) error {
+	reviewsDB := os.Getenv("REVIEWS_DB")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(reviewsDB))
+
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": err})
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	//ping primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": err})
+	}
+
+	db := client.Database("user_reviews").Collection("reviews")
+
+	cur, err := db.Find(context.TODO(), bson.D{})
+
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": err})
+	}
+	var results []ResultStruct
+
+	for cur.Next(context.TODO()) {
+		var elem ResultStruct
+		err := cur.Decode(&elem)
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{"status": err})
+		}
+		results = append(results, elem)
+	}
+
+	if err := cur.Err(); err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": err})
+	}
+
+	cur.Close(context.TODO())
+	return c.Status(200).JSON(results)
+}
+
+func PostTag(c *fiber.Ctx) error {
+	review := new(PostStruct)
+	err := c.BodyParser(review)
+
+	for i, tag := range review.Tags {
+		review.Tags[i] = "#" + tag
+	}
+
+	//connect to the database
+	reviewsDB := os.Getenv("REVIEWS_DB")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(reviewsDB))
+
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": err})
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	//ping primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": err})
+	}
+
+	db := client.Database("user_reviews").Collection("reviews")
+
+	res, err := db.InsertOne(context.TODO(), review)
+
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"status": err})
+	}
+
+	return c.Status(200).JSON(fiber.Map{"insert_id": res.InsertedID})
 }
--- microservices/reviews/routes/routes.go
+++ microservices/reviews/routes/routes.go
@@ -2,12 +2,10 @@ package routes
 
 import (
 	//	"fmt"
-	"strings"
 
 	"github.com/alexishamrak/xplored-reviews/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/kr/pretty"
 )
 
 func RegisterRoutes(app *fiber.App) {
@@ -17,41 +15,8 @@ func RegisterRoutes(app *fiber.App) {
 	microservice := app.Group("/reviews") // /api
 	api := microservice.Group("/api")
 	v1 := api.Group("/v1")
-	// v1.Get("/tags", handlers.SearchTag)
-
-	reviewsRoutes(v1)
-}
-
-func reviewsRoutes(v fiber.Router) {
-	println("inside reviews routes")
-	v.Get("/:tags", GetReviewsOptions)
-}
-
-/*
-* Function associated with Endpoint to get Reviews Data
- */
-func GetReviewsOptions(c *fiber.Ctx) error {
-	println("Inside get reviews options")
-	data := reviewsOptions(c, c.Params("tags"))
-
-	errorStatus := "Success"
-
-	if len(data.Error()) <= 0 {
-		errorStatus = "Error"
-	}
-
-	return c.Status(200).JSON(fiber.Map{"status": errorStatus, "data": data})
-}
-
-/*
-* reviewsOptions with Given Information
- */
-func reviewsOptions(c *fiber.Ctx, tags string) error {
-	//Format Address
-	plusFormattedTags := strings.ReplaceAll("+"+tags, " ", "+")
-	println("inside reviews options")
-	//Get Reviews Data
-	Result := handlers.SearchTag(c, plusFormattedTags)
-	pretty.Println("The result is %s", Result) //Debugging Print Statement
-	return Result
+	v1.Get("/tags", handlers.SearchTag)
+	v1.Post("/tag", handlers.PostTag)
+	v1.Get("/alltags", handlers.AllReviews)
+	// reviewsRoutes(v1)
 }
