9e721b56c54391b78efdd2b0697318ba3946d05e
Robert Brown <brown.rjb75@gmail.com>
2022-03-17 09:00:35 -0600

    feat: add initial styles and colors for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./styles/main.scss"
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import ComponentTest from "./pages/ComponentTest";
