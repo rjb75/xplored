9359ae5257cace75b5a640b482f7ace60fc093c1
nickknapton12 <nickknapton12@gmail.com>
2022-03-18 10:33:58 -0600

    fonts + minor fix

--- frontend/src/pages/ComponentTest.tsx
+++ frontend/src/pages/ComponentTest.tsx
@@ -2,25 +2,19 @@ import React from "react";
 import { Accommodation } from "../components/Accommodation";
 import { Auth } from "../components/Auth";
 import { Dining } from "../components/Dining";
+import EventCard from "../components/EventCard";
 import { PhotoGallery } from "../components/PhotoGallery";
 import { POI } from "../components/POI";
 import { Ratings } from "../components/Ratings";
 import { Recommendations } from "../components/Recommendations";
 import { Transportation } from "../components/Transportation";
 import { TravelPlanner } from "../components/TravelPlanner";
+import { eventTypes } from "../components/EventCard";
 
 function ComponentTest() {
     return (
-        <div>
-            <Accommodation />
-            <Auth />
-            <Dining />
-            <PhotoGallery />
-            <POI />
-            <Ratings />
-            <Recommendations />
-            <Transportation />
-            <TravelPlanner />
+        <div style={{height:`40%`, width: `40%`, margin:`auto`}}>
+            <EventCard  color="pink" title="Test event" time="8:25 am" type={eventTypes.FOOD} duration="1 hour"/>
         </div>
     );
 }
