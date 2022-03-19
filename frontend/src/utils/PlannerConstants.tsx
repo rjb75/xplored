bda61bd8dfe7b527fc64859cc5e2c4c1bf7704b6
nickknapton12 <nickknapton12@gmail.com>
2022-03-18 18:17:52 -0600

    Table done

---
+++ frontend/src/utils/PlannerConstants.tsx
@@ -0,0 +1,146 @@
+export const days: { name: String }[] = [
+  {
+    name: "Monday",
+  },
+  {
+    name: "Tuesday",
+  },
+  {
+    name: "Whensday",
+  },
+  {
+    name: "Thursday",
+  },
+  {
+    name: "Friday",
+  },
+  {
+    name: "Saturday",
+  },
+  {
+    name: "Sunday",
+  },
+];
+
+export const timeRows: { time: String }[] = [
+  {
+    time: "6 am",
+  },
+  {
+    time: "6:30 am",
+  },
+  {
+    time: "7 am",
+  },
+  {
+    time: "7:30 am",
+  },
+  {
+    time: "8 am",
+  },
+  {
+    time: "8:30 am",
+  },
+  {
+    time: "9 am",
+  },
+  {
+    time: "9:30 am",
+  },
+  {
+    time: "10 am",
+  },
+  {
+    time: "10:30 am",
+  },
+  {
+    time: "11 am",
+  },
+  {
+    time: "11:30 am",
+  },
+  {
+    time: "12 pm",
+  },
+  {
+    time: "12:30 pm",
+  },
+  {
+    time: "1 pm",
+  },
+  {
+    time: "1:30 pm",
+  },
+  {
+    time: "2 pm",
+  },
+  {
+    time: "2:30 pm",
+  },
+  {
+    time: "3 pm",
+  },
+  {
+    time: "3:30 pm",
+  },
+  {
+    time: "4 pm",
+  },
+  {
+    time: "4:30 pm",
+  },
+  {
+    time: "5 pm",
+  },
+  {
+    time: "5:30 pm",
+  },
+  {
+    time: "6 pm",
+  },
+  {
+    time: "6:30 pm",
+  },
+  {
+    time: "7 pm",
+  },
+  {
+    time: "7:30 pm",
+  },
+  {
+    time: "8 pm",
+  },
+  {
+    time: "8:30 pm",
+  },
+  {
+    time: "9 pm",
+  },
+  {
+    time: "9:30 pm",
+  },
+  {
+    time: "10 pm",
+  },
+  {
+    time: "10:30 pm",
+  },
+  {
+    time: "11 pm",
+  },
+  {
+    time: "11:30 pm",
+  },
+  {
+    time: "12 am",
+  },
+  {
+    time: "12:30 am",
+  },
+  {
+    time: "1 am",
+  },
+  {
+    time: "1:30 am",
+  },
+];
