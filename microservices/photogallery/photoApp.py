f2cce2bf3a2e9a78a0d67a07ba22f9f849f37b3d
Amnah Hussain <73035499+Amnah736@users.noreply.github.com>
2022-03-20 14:35:35 -0600

    feat: updated photo gallery to get different sized photos

--- microservices/photogallery/photoApp.py
+++ microservices/photogallery/photoApp.py
@@ -7,14 +7,35 @@ photoApp = FastAPI()
 @photoApp.get("/")
 def index():
     return {"message": "Hello World"}
-
-#obtains one photo according to the photo name put
+    
+#photo types:
+# raw, full, regular, small, thumb, small_s3
+#obtains one photo according to the photo name and takes in an optional query for photo size
 @photoApp.get("/photo/api/v1/{photo_name}")
-def get_photo(photo_name):
+def get_photo(photo_name, photoType: str | None = None):
     photos = config.api.search.photos(photo_name, per_page=1, page=1)['results']
-    for photo in photos:
-        return {"photo_id": photo.id,
-            "photo_url": photo.urls.raw}
+    for photo in photos:    
+        if(photoType == "raw"):
+            return {"photo_id": photo.id,
+                    "photo_url": photo.urls.raw}
+        elif(photoType == "full"):
+            return {"photo_id": photo.id,
+                    "photo_url": photo.urls.full}
+        elif(photoType == "regular"):
+            return {"photo_id": photo.id,
+                    "photo_url": photo.urls.regular}
+        elif(photoType == "small"):
+            return {"photo_id": photo.id,
+                    "photo_url": photo.urls.small}
+        elif(photoType == "thumb"):
+            return {"photo_id": photo.id,
+                    "photo_url": photo.urls.thumb}
+        elif(photoType == "small_s3"):
+            return {"photo_id": photo.id,
+                    "photo_url": photo.urls.small_s3}
+        else:
+            return {"photo_id": photo.id,
+                    "photo_url": photo.urls.full}
 
 if __name__ == "__main__":
     uvicorn.run(photoApp, host=os.getenv("PHOTO_HOST"), port=int(os.getenv("PHOTO_PORT")))
