441b5c5184c988f8d4984680ef36fe9828e7f010
nickknapton12 <nickknapton12@gmail.com>
2022-03-20 22:15:47 -0600

    Somewhat works

---
+++ frontend/src/components/PlannerCell.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { useDrop } from 'react-dnd';
+import { ItemTypes } from "../utils/PlannerConstants";
+import EventCard, { eventTypes, plannerEvent } from "./EventCard";
+
+type AppProps = {
+    day: String,
+    time: number,
+    children?: JSX.Element,
+    test?: boolean,
+}
+
+export const PlannerCell = ({day, time, children, test}: AppProps) => {
+    const [event, setEvents] = useState<plannerEvent | undefined>();
+
+    function moveEvent(item: plannerEvent){
+        item.cleanupFunction();
+        setEvents(item);
+    }
+
+    function removeEvent(item: plannerEvent){
+        console.log(test)
+    }
+
+    function ifEvent(){
+        if(event !== undefined || test){
+            return(
+                <EventCard type={event?.type || eventTypes.FOOD} title={event?.title || "Test"} time={event?.time || "6:30 pm"}  duration={event?.duration || "1 Hour"} date={event?.date || "0"} cleanupFunction={removeEvent}  />
+            );
+        }
+    }
+
+    const [{ isOver }, drop] = useDrop(() => ({
+        accept: ItemTypes.EVENT,
+        drop: (item: plannerEvent) => moveEvent(item),
+        collect: monitor => ({
+            isOver: !!monitor.isOver(),
+        }),
+    }), [day, time])
+
+    return(
+        <div ref={drop} className="cell">{ifEvent()}</div>
+    );
+}
+
+export default PlannerCell;
