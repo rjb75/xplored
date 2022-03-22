77c1cab5cd6ec77a5af98b11af2e7456a095e515
nickknapton12 <nickknapton12@gmail.com>
2022-03-22 10:18:14 -0600

    Icons and colors added for event cards

--- frontend/src/utils/PlannerConstants.tsx
+++ frontend/src/utils/PlannerConstants.tsx
@@ -30,121 +30,121 @@ export const days: { name: String }[] = [
 
 export const timeRows: { time: String }[] = [
   {
-    time: "6 am",
+    time: "6:00 am",
   },
   {
     time: "6:30 am",
   },
   {
-    time: "7 am",
+    time: "7:00 am",
   },
   {
     time: "7:30 am",
   },
   {
-    time: "8 am",
+    time: "8:00 am",
   },
   {
     time: "8:30 am",
   },
   {
-    time: "9 am",
+    time: "9:00 am",
   },
   {
     time: "9:30 am",
   },
   {
-    time: "10 am",
+    time: "10:00 am",
   },
   {
     time: "10:30 am",
   },
   {
-    time: "11 am",
+    time: "11:00 am",
   },
   {
     time: "11:30 am",
   },
   {
-    time: "12 pm",
+    time: "12:00 pm",
   },
   {
     time: "12:30 pm",
   },
   {
-    time: "1 pm",
+    time: "1:00 pm",
   },
   {
     time: "1:30 pm",
   },
   {
-    time: "2 pm",
+    time: "2:00 pm",
   },
   {
     time: "2:30 pm",
   },
   {
-    time: "3 pm",
+    time: "3:00 pm",
   },
   {
     time: "3:30 pm",
   },
   {
-    time: "4 pm",
+    time: "4:00 pm",
   },
   {
     time: "4:30 pm",
   },
   {
-    time: "5 pm",
+    time: "5:00 pm",
   },
   {
     time: "5:30 pm",
   },
   {
-    time: "6 pm",
+    time: "6:00 pm",
   },
   {
     time: "6:30 pm",
   },
   {
-    time: "7 pm",
+    time: "7:00 pm",
   },
   {
     time: "7:30 pm",
   },
   {
-    time: "8 pm",
+    time: "8:00 pm",
   },
   {
     time: "8:30 pm",
   },
   {
-    time: "9 pm",
+    time: "9:00 pm",
   },
   {
     time: "9:30 pm",
   },
   {
-    time: "10 pm",
+    time: "10:00 pm",
   },
   {
     time: "10:30 pm",
   },
   {
-    time: "11 pm",
+    time: "11:00 pm",
   },
   {
     time: "11:30 pm",
   },
   {
-    time: "12 am",
+    time: "12:00 am",
   },
   {
     time: "12:30 am",
   },
   {
-    time: "1 am",
+    time: "1:00 am",
   },
   {
     time: "1:30 am",
