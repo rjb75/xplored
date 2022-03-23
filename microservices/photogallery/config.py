f2fa5cc44e576625d244eab75d34b8f0efa94a11
Amnah Hussain <73035499+Amnah736@users.noreply.github.com>
2022-03-23 00:01:54 -0600

    feat: removed need for unsplash dependency

--- microservices/photogallery/config.py
+++ microservices/photogallery/config.py
@@ -1,19 +1,8 @@
-from unsplash.api import Api
-from unsplash.auth import Auth
 from dotenv import load_dotenv
-import os
+import os, requests
 
 load_dotenv()
 config = load_dotenv(".env")
 
 client_id = os.getenv("PHOTO_CLIENT_ID")
-client_secret = os.getenv("PHOTO_CLIENT_SECRET")
-redirect_uri = os.getenv("PHOTO_DIRECT_URI")
-code = ""
-
-#connecting to unsplash api
-auth = Auth(client_id, client_secret, redirect_uri, code=code)
-api = Api(auth)
-
-
-
+photo_url = "https://api.unsplash.com/search/photos?client_id="+client_id
--- microservices/photogallery/photoApp.py
+++ microservices/photogallery/photoApp.py
@@ -1,4 +1,4 @@
-import config, uvicorn, os
+import config, uvicorn, os, requests
 from fastapi import FastAPI
 
 photoApp = FastAPI()
@@ -14,33 +14,26 @@ def index():
 #takes in optional query for photo height and width as well
 @photoApp.get("/photo/api/v1/{photo_name}")
 def get_photo(photo_name, width: int | None = None, height: int | None = None, photoType: str | None = None):
-    photos = config.api.search.photos(photo_name, per_page=1, page=1)['results']
-    for photo in photos:
-        if None not in (width, height):
-            photoUrl = "https://source.unsplash.com/{photo_ID}/{width}x{height}".format(photo_ID = str(photo.id), width = str(width), height = str(height))
-            return {"photo_id" : photo.id, 
-                    "photo_url" : photoUrl}    
-        elif(photoType == "raw"):
-            return {"photo_id": photo.id,
-                    "photo_url": photo.urls.raw}
-        elif(photoType == "full"):
-            return {"photo_id": photo.id,
-                    "photo_url": photo.urls.full}
-        elif(photoType == "regular"):
-            return {"photo_id": photo.id,
-                    "photo_url": photo.urls.regular}
-        elif(photoType == "small"):
-            return {"photo_id": photo.id,
-                    "photo_url": photo.urls.small}
-        elif(photoType == "thumb"):
-            return {"photo_id": photo.id,
-                    "photo_url": photo.urls.thumb}
-        elif(photoType == "small_s3"):
-            return {"photo_id": photo.id,
-                    "photo_url": photo.urls.small_s3}
-        else:
-            return {"photo_id": photo.id,
-                    "photo_url": photo.urls.full}
+    #create query for photo
+    querystring = {"query":photo_name,"page":"1", "per_page":"1"}
+    #send GET request to unsplash api
+    response = requests.request("GET", config.photo_url, params=querystring)
+    
+    results_dict = response.json()['results'][0]
+    urls_dict = results_dict['urls']
+
+    photo_id = results_dict['id']
+
+    if None not in (width, height):
+        photoUrl = "https://source.unsplash.com/{photo_ID}/{width}x{height}".format(photo_ID = str(photo_id), width = str(width), height = str(height))
+        return {"photo_id" : photo_id, 
+                "photo_url" : photoUrl}    
+    elif photoType is not None:
+        return {"photo_id": photo_id,
+                "photo_url": urls_dict[photoType]}
+    else:
+        return {"photo_id": photo_id,
+                "photo_url": urls_dict['full']}
 
 if __name__ == "__main__":
     uvicorn.run(photoApp, host=os.getenv("PHOTO_HOST"), port=int(os.getenv("PHOTO_PORT")))
