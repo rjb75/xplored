a3f04957322a7f9ed558bf3a5255087979573e31
alexishamrak <alhamrak@shaw.ca>
2022-03-23 19:32:47 -0600

    started writing transportation framework

---
+++ microservices/transportation/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	app := fiber.New()
+
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	//Configure all Routes
+	routes.RegisterRoutes(app)
+
+	//Formatting port
+	PLANNER_PORT := os.Getenv("TPLANNER_PORT")
+	port := fmt.Sprintf(":%s", PLANNER_PORT)
+	app.Listen(port)
+
+}
