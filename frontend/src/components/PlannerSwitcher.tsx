c31e5e48a3c2cf24f4bec22b4f271faffba11eb9
nickknapton12 <nickknapton12@gmail.com>
2022-03-23 19:59:02 -0600

    Final

--- frontend/src/components/PlannerSwitcher.tsx
+++ frontend/src/components/PlannerSwitcher.tsx
@@ -16,7 +16,7 @@ export const PlannerSwitcher = () => {
   }
 
   return (
-    <Draggable>
+    <Draggable defaultPosition={{x: 1800, y: -800}} bounds={{}}>
       <div className="PlannerSwitcherWrapper">
         <div
           className={
