e33f9715f059bb31c16402d0aeeea4c2a33277df
Brooke Kindleman <brooke.kindleman@gmail.com>
2022-03-25 16:01:49 -0600

    docs: Removed unused imports in ComponentTest

--- frontend/src/pages/ComponentTest.tsx
+++ frontend/src/pages/ComponentTest.tsx
@@ -1,18 +1,5 @@
-import { Sign } from "crypto";
-import React from "react";
-import { Accommodation } from "../components/Accommodation";
-import { Auth } from "../components/Auth";
-import { Dining } from "../components/Dining";
-import EventCard from "../components/EventCard";
-import { PhotoGallery } from "../components/PhotoGallery";
-import { POI } from "../components/POI";
-import { Ratings } from "../components/Ratings";
-import { Recommendations } from "../components/Recommendations";
-import { Transportation } from "../components/Transportation";
 import { TravelPlanner } from "../components/TravelPlanner";
-import { eventTypes } from "../components/EventCard";
 import TravelMap from "../components/TravelMap";
-import PlannerSwitcher from "../components/PlannerSwitcher";
 import NavBar from "../components/NavBar";
 
 function ComponentTest() {
