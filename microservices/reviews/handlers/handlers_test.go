0a1bf9c481a5f3a382fdfce11a5244fcfc5ae123
Robert Brown <brown.rjb75@gmail.com>
2022-03-25 21:55:58 -0600

    feat: update reviews unit tests

--- microservices/reviews/handlers/handlers_test.go
+++ microservices/reviews/handlers/handlers_test.go
@@ -45,3 +45,71 @@ func TestAllReviews(t *testing.T) {
 	assert.NotNil(t, body, "Reviews should be returned")
 
 }
+
+func TestValidTagReviews(t *testing.T) {
+	err := godotenv.Load("../../../.env")
+
+	if err != nil {
+		assert.Fail(t, "Error loading .env file")
+	}
+
+	handlers.DBConnect()
+
+	app := fiber.New()
+	app.Get("/", handlers.SearchTag)
+
+	req, err := http.NewRequest("GET", "/?tags=paris", nil)
+
+	if err != nil {
+		assert.Fail(t, "Error forming test request")
+	}
+
+	res, err := app.Test(req, -1)
+
+	if err != nil {
+		assert.Fail(t, "Error making test request")
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		assert.Fail(t, "Error parsing test response")
+	}
+
+	assert.NotNil(t, body, "Reviews should be returned")
+	assert.NotEqual(t, "null", string(body), "Result should not be null")
+
+}
+
+func TestInvalidTagReviews(t *testing.T) {
+	err := godotenv.Load("../../../.env")
+
+	if err != nil {
+		assert.Fail(t, "Error loading .env file")
+	}
+
+	handlers.DBConnect()
+
+	app := fiber.New()
+	app.Get("/", handlers.SearchTag)
+
+	req, err := http.NewRequest("GET", "/?tags=horseing", nil)
+
+	if err != nil {
+		assert.Fail(t, "Error forming test request")
+	}
+
+	res, err := app.Test(req, -1)
+
+	if err != nil {
+		assert.Fail(t, "Error making test request")
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		assert.Fail(t, "Error parsing test response")
+	}
+
+	assert.Equal(t, string(body), "null", "No reviews should have been returned")
+}
