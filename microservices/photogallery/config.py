72296c59ee6ab7707014f23fa9c6ea77f3f67674
Amnah Hussain <73035499+Amnah736@users.noreply.github.com>
2022-03-27 15:32:55 -0600

    chore: added comments to photo gallery files

--- microservices/photogallery/config.py
+++ microservices/photogallery/config.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv
 import os, requests
 
+# loads env file
 load_dotenv()
 config = load_dotenv(".env")
 
+# gets client ID from env file
 client_id = os.getenv("PHOTO_CLIENT_ID")
+# initializes url for sending GET requests to fastapi
 photo_url = "https://api.unsplash.com/search/photos?client_id="+client_id
 random_url = "https://api.unsplash.com/photos/random?client_id="+client_id
--- microservices/photogallery/photoApp.py
+++ microservices/photogallery/photoApp.py
@@ -10,37 +10,53 @@ def index():
 
 #photo types:
 #raw, full, regular, small, thumb, small_s3
-#obtains one photo according to the photo name and takes in an optional query for photo size
-#takes in optional query for photo height and width as well
+
+# The function get_photo queries unsplash api for pictures that relate to the user-inputted
+# name query and returns an image url accordingly.
+# The function takes in optional queries of dimensions (width, height) to return a url containing
+# the image with the user-inputted dimensions.
+# The function also takes in more optional queries for the photo type returned, the image can be
+# returned in a photo type of 'raw', 'full', 'regular', 'small', 'thumb' or 'small_s3'.
 @photoApp.get("/photo/api/v1/photo")
 def get_photo(name: str, width: int | None = None, height: int | None = None, photoType: str | None = None):
-    #create query for photo
+    # create query for photo
     querystring = {"query":name,"page":"1", "per_page":"5"}
-    #send GET request to unsplash api
+    # send GET request to unsplash api
     response = requests.request("GET", config.photo_url, params=querystring)
-
+    # stores response from unsplash api in a dictionary
     results_dict = response.json()['results'][0]
+    # stores the given urls
     urls_dict = results_dict['urls']
-
+    # stores the given photo ID
     photo_id = results_dict['id']
 
+    # returns photo url in given dimensions if width and height queries contain integers
     if None not in (width, height):
         photoUrl = "https://source.unsplash.com/{photo_ID}/{width}x{height}".format(photo_ID = str(photo_id), width = str(width), height = str(height))
         return {"photo_id" : photo_id,
                 "photo_url" : photoUrl}
+    # returns photo url of given photo type if photoType query contains a string
     elif photoType is not None:
         return {"photo_id": photo_id,
                 "photo_url": urls_dict[photoType]}
+    # returns a photo url of photo type 'full' if the optional queries contain nothing
     else:
         return {"photo_id": photo_id,
                 "photo_url": urls_dict['full']}
 
+# The function get_random_photo queries unsplash api for pictures that relate to the user-inputted
+# name query and returns an image url accordingly. Each time the function is called a differnet
+# url is returned so that the same photo is not returned each time.
 @photoApp.get("/photo/api/v1/random")
 def get_random_photo(name: str):
+    # create query for photo
     querystring = {"query": name, "count": "1"}
+    # send GET request to unsplash api
     response = requests.request("GET", config.random_url, params=querystring)
+    # stores response from unsplash api in a dictionary
     random_dict = response.json()[0]
 
+    #returns photo url
     return {"url": random_dict['urls']['full']}
 
 if __name__ == "__main__":
