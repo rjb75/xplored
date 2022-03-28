d585fbc1243a69ada9a0bb30d28eddadb9db22e7
Dylan Wheeler <dwheeler1127@gmail.com>
2022-03-27 23:49:46 -0600

    feat: added short-distance transportation card css

--- frontend/src/components/CarCard.tsx
+++ frontend/src/components/CarCard.tsx
@@ -1,17 +1,59 @@
 import React from "react";
 import "./EventCard.scss";
+import CarIcon from "../assets/carIcon2.svg";
+import BikeIcon from "../assets/bikeIcon.svg";
+import ChevronSmall from "../assets/chevron-small.svg";
 
 interface CarCardProps {
-    departureLocation?: string;
-    arrivalLocation?: string;
-    date?: string[];
-    time?: string;
+    type?: string;
+    duration?: string;
+    distance?: string;
+    link?: string;
 }
 
 export default function CarCard(props: CarCardProps) {
+    let logo;
+    let linkLabel;
+    switch (props.type) {
+        case "driving":
+            logo = CarIcon;
+            linkLabel = "Book Taxi";
+            break;
+        case "transit":
+            logo = CarIcon;
+            linkLabel = "Get Tickets";
+            break;
+        case "cycling":
+            logo = BikeIcon;
+            linkLabel = "Find Rentals";
+            break;
+        case "walking":
+            logo = BikeIcon;
+            break;
+        default:
+            break;
+    }
+
     return (
         <div className="card--entry card--car">
-            <h1>This is a car card</h1>
+            <img
+                className="card--car-icon"
+                src={logo}
+                alt={props.type + " transportation"}
+            />
+            <div className="card--car-info-holder">
+                <h1 className="card--car-duration">{props.duration}</h1>
+                <h2 className="card--car-distance">{props.distance}</h2>
+                {props.link && linkLabel && (
+                    <a className="card--car-link" href={props.link}>
+                        {linkLabel}
+                    </a>
+                )}
+            </div>
+            <a href="#" className="card--car-directions">
+                <p>Directions</p>
+                <img src={ChevronSmall} alt="view directions" />
+            </a>
         </div>
     );
 }
