75febc2756c5de2f8a167ce4fc897eff9f076182
alexishamrak <alhamrak@shaw.ca>
2022-03-28 22:36:27 -0600

    added functionality for lat long

--- microservices/dining/models/models.go
+++ microservices/dining/models/models.go
@@ -5,3 +5,10 @@ type DiningRequest struct {
 	Keyword string `json:"keyword"`
 	Radius  string `json:"radius"`
 }
+
+type Coordinates struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lng"`
+	Radius    int     `json:"radius"`
+	Keyword   string  `json:"keyword"`
+}
