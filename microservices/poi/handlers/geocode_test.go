ff267052bd1a7e65c6e010561e9c7d791caba940
Robert Brown <brown.rjb75@gmail.com>
2022-03-28 18:01:24 -0600

    feat: fix poi issues with invalid address

--- microservices/poi/handlers/geocode_test.go
+++ microservices/poi/handlers/geocode_test.go
@@ -37,3 +37,29 @@ func Test_latlon_UofC(t *testing.T) {
 	utils.AssertEqual(t, 51.0755455, loc.Lat, "Latitude")
 	utils.AssertEqual(t, -114.1297544, loc.Lng, "Longitude")
 }
+
+func Test_latlon_Invalid(t *testing.T) {
+
+	logs.TestLogs()
+
+	err := godotenv.Load("../../../.env")
+
+	if err != nil {
+		logs.ErrorLogger.Fatalln("Couldn't load environment file")
+	}
+
+	client, err := maps.NewClient(maps.WithAPIKey(os.Getenv("POI_GOOGLE_API_KEY")))
+
+	if err != nil {
+		logs.ErrorLogger.Fatalln("Error connecting with api")
+	}
+
+	request := models.POIRequest{
+		Address: "123 Sesame Street",
+	}
+
+	loc := handlers.Latlon(&request, client)
+
+	utils.AssertEqual(t, 0.0, loc.Lat, "Lat should be 0.0")
+	utils.AssertEqual(t, 0.0, loc.Lng, "Lng should be 0.0")
+}
